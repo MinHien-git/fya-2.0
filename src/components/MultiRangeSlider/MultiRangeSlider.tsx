2df34103f499abb50b1e80e189eb6ab9aefa0b10
MinHien <64275316+MinHien-git@users.noreply.github.com>
2024-01-19 20:56:52 +0700

    update commit

--- src/components/MultiRangeSlider/MultiRangeSlider.tsx
+++ src/components/MultiRangeSlider/MultiRangeSlider.tsx
@@ -1,55 +1,99 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 import "./index.css";
 import clsx from "clsx";
 
 export default function MultiRangeSlider() {
   const [minVal, setMinVal] = useState(0);
   const [maxVal, setMaxVal] = useState(1000);
-  const minValRef = useRef(null);
-  const maxValRef = useRef(null);
+  const minValRef: any = useRef(null);
+  const maxValRef: any = useRef(null);
+  const range: any = useRef(null);
+
+  const getPercent = useCallback(
+    (value: any) => Math.round(((value - 0) / (1000 - 0)) * 100),
+    [0, 1000]
+  );
+
+  // Set width of the range to decrease from the left side
+  useEffect(() => {
+    if (maxValRef.current) {
+      const minPercent = getPercent(minVal);
+      const maxPercent = getPercent(+maxValRef.current.value);
+
+      if (range.current) {
+        range.current.style.left = `${minPercent}%`;
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }
+  }, [minVal, getPercent]);
+  // Set width of the range to decrease from the right side
+  useEffect(() => {
+    if (minValRef.current) {
+      const minPercent = getPercent(+minValRef.current.value);
+      const maxPercent = getPercent(maxVal);
+
+      if (range.current) {
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }
+  }, [maxVal, getPercent]);
 
   return (
-    <>
-      <label htmlFor="left" className="hidden">
-        left
-      </label>
-      <label htmlFor="right" className="hidden">
-        right
-      </label>
-      <input
-        type="range"
-        min={0}
-        max={1000}
-        value={minVal}
-        ref={minValRef}
-        id="left"
-        onChange={(event) => {
-          const value = Math.min(+event.target.value, maxVal - 1);
-          setMinVal(value);
-          event.target.value = value.toString();
-        }}
-        className={clsx("thumb thumb--zindex-3", {
-          "thumb--zindex-5": minVal > 1000 - 100,
-        })}
-      />
-      <input
-        type="range"
-        id="right"
-        min={0}
-        max={1000}
-        value={maxVal}
-        ref={maxValRef}
-        onChange={(event) => {
-          const value = Math.max(+event.target.value, minVal + 1);
-          setMaxVal(value);
-          event.target.value = value.toString();
-        }}
-        className="thumb thumb--zindex-4"
-      />
-      <div className="slider">
+    <div className="border-primary slider pt-5 pb-8 border-2 flex justify-center rounded-xl px-5 bg-white lg:pt-0 lg:pb-1">
+      <div className="slider pt-5 pb-8 flex justify-center relative">
+        <label htmlFor="left" className="hidden">
+          left
+        </label>
+        <label htmlFor="right" className="hidden">
+          right
+        </label>
+        <input
+          type="range"
+          min={0}
+          max={1000}
+          value={minVal}
+          ref={minValRef}
+          id="left"
+          onChange={(event) => {
+            const value = Math.min(+event.target.value, maxVal - 1);
+            setMinVal(value);
+            event.target.value = value.toString();
+          }}
+          className={clsx("thumb thumb--zindex-3", {
+            "thumb--zindex-5": minVal > 1000 - 100,
+          })}
+        />
+        <input
+          type="range"
+          id="right"
+          min={0}
+          max={1000}
+          value={maxVal}
+          ref={maxValRef}
+          onChange={(event) => {
+            const value = Math.max(+event.target.value, minVal + 1);
+            setMaxVal(value);
+            event.target.value = value.toString();
+          }}
+          className="thumb thumb--zindex-4"
+        />
         <div className="slider__track" />
-        <div className="slider__range" />
+        <div className="slider__range" ref={range} />
+        <ul className="flex w-full absolute justify-between pt-12">
+          <li className="font-bold">$0</li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="w-[2px] h-[8px] bg-gray-400 mt-2"></li>
+          <li className="font-bold">$1M</li>
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
--- src/pages/SearchResult/SearchResult.tsx
+++ src/pages/SearchResult/SearchResult.tsx
@@ -2,29 +2,120 @@ import MultiRangeSlider from "../../components/MultiRangeSlider/MultiRangeSlider
 
 export default function SearchResult() {
   return (
-    <main className="w-full min-h-80 p-2">
-      <section className="w-full border-2 max-w-7xl mb-5 rounded-b-3xl md:mx-auto min-h-[40vh] bg-light_yellow">
-        <div className="flex gap-3 px-2">
-          <div className="bg-tertiary text-primary font-bold px-16 py-4">
-            result filter
+    <main className="w-full min-h-80 p-2 pt-0">
+      <section className="w-full border-2 max-w-7xl mb-5 pb-5 rounded-b-3xl md:mx-auto min-h-[40vh] bg-light_yellow px-3">
+        <div className="grid gap-3 px-2 h-auto py-5">
+          <div className="bg-tertiary text-primary font-bold px-16 py-4 text-center">
+            Result Filters:
           </div>
-          <div className="flex flex-wrap">
-            <div className="grid">
-              <label htmlFor="services">Services</label>
-              <input type="text" id="services" />
+          <div className="grid flex-wrap gap-5 md:flex md:justify-start items-start">
+            <div className="grid gap-3 w-1/4">
+              <label
+                htmlFor="services"
+                className="bg-secondary font-semibold w-fit px-6 py-[0.225rem] md:py-[0.425rem] text-sm rounded-md"
+              >
+                Services
+              </label>
+              <input
+                type="text"
+                id="services"
+                className="border-primary border-2 p-2 rounded-md"
+              />
             </div>
-            <div className="grid">
-              <label htmlFor="experience">Having experience in</label>
-              <input type="text" id="experience" />
+            <div className="grid gap-3">
+              <label
+                htmlFor="experience"
+                className="bg-secondary font-semibold w-fit px-6 py-[0.225rem] md:py-[0.425rem] text-sm rounded-md"
+              >
+                Having experience in
+              </label>
+              <input
+                type="text"
+                id="experience"
+                className="border-primary border-2 p-2 rounded-md"
+                placeholder="Select industries"
+              />
             </div>
 
-            <div className="grid">
-              <label htmlFor="location">Location</label>
-              <input type="text" id="location" />
+            <div className="grid gap-3">
+              <label
+                htmlFor="location"
+                className="bg-secondary font-semibold w-fit px-6 py-[0.225rem] md:py-[0.425rem] text-sm rounded-md"
+              >
+                Location
+              </label>
+              <input
+                type="text"
+                id="location"
+                className="border-primary border-2 p-2 rounded-md"
+                placeholder="Type location"
+              />
             </div>
-            <div className="grid">
-              <label htmlFor="budget">Estimated budget </label>
+            <div className="grid gap-3 md:w-full lg:w-1/3">
+              <label
+                htmlFor="budget"
+                className="bg-secondary font-semibold w-fit px-6 py-[0.225rem] md:py-[0.425rem] text-sm rounded-md "
+              >
+                Estimated budget{" "}
+              </label>
               <MultiRangeSlider />
+            </div>
+          </div>
+          <div className="grid md:flex flex-wrap gap-5 md:justify-start">
+            <div className="grid gap-3 w-1/4">
+              <label
+                htmlFor="services"
+                className="bg-secondary font-semibold w-fit px-6 py-[0.225rem] md:py-[0.425rem] text-sm rounded-md"
+              >
+                Skills
+              </label>
+              <input
+                type="text"
+                id="services"
+                className="border-primary border-2 p-2 rounded-md"
+              />
+            </div>
+            <div className="grid gap-3">
+              <label
+                htmlFor="services"
+                className="bg-secondary font-semibold w-fit px-6 py-[0.225rem] md:py-[0.425rem] text-sm rounded-md"
+              >
+                Language(s)
+              </label>
+              <input
+                type="text"
+                id="services"
+                className="border-primary border-2 p-2 rounded-md"
+                placeholder="Select language(s)"
+              />
+            </div>
+            <div className="grid gap-3">
+              <label
+                htmlFor="services"
+                className="bg-secondary font-semibold w-fit px-6 py-[0.225rem] md:py-[0.425rem] text-sm rounded-md"
+              >
+                Team size
+              </label>
+              <input
+                type="text"
+                id="services"
+                className="border-primary border-2 p-2 rounded-md"
+                placeholder="Select a size"
+              />
+            </div>
+            <div className="grid gap-3">
+              <label
+                htmlFor="services"
+                className="bg-secondary font-semibold w-fit px-6 py-[0.225rem] md:py-[0.425rem] text-sm rounded-md"
+              >
+                Sort by
+              </label>
+              <input
+                type="text"
+                id="services"
+                className="border-primary border-2 p-2 rounded-md"
+                placeholder="Recommended"
+              />
             </div>
           </div>
         </div>
