f0a8907efb0068500a4db0b73d833ef5852d6770
MinHien <64275316+MinHien-git@users.noreply.github.com>
2024-01-26 13:38:42 +0700

    update

--- src/pages/SearchResult/SearchResult.tsx
+++ src/pages/SearchResult/SearchResult.tsx
@@ -5,7 +5,7 @@ export default function SearchResult() {
     <main className="w-full min-h-80 p-2 pt-0">
       <section className="w-full border-2 max-w-7xl mb-5 pb-5 rounded-b-3xl md:mx-auto min-h-[40vh] bg-light_yellow px-3">
         <div className="grid gap-3 px-2 h-auto py-5">
-          <div className="bg-tertiary text-primary font-bold px-16 py-4 text-center">
+          <div className="bg-tertiary text-primary font-bold px-16 py-4 text-center font-title">
             Result Filters:
           </div>
           <div className="grid flex-wrap gap-5 md:flex md:justify-start items-start">
