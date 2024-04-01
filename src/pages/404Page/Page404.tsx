3f3693ef60fd840dcc10c5c394e687983b63f7b0
MinHien <64275316+MinHien-git@users.noreply.github.com>
2024-04-01 23:00:39 +0700

    update

--- src/pages/404Page/Page404.tsx
+++ src/pages/404Page/Page404.tsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 export default function Page404() {
   return (
     <main className="w-full h-[100vh] flex justify-center items-center flex-col">
-      <div className="h-[70vh] w-full flex flex-col items-center justify-center gap-4">
+      <div className=" w-full flex flex-col items-center justify-center gap-4">
         <img
-          src="/static/images/404 Error with a cute animal-amico.svg"
+          src="/static/images/404 Error with a cute animal-pana.svg"
           alt="Update feature"
-          className="w-1/2"
+          className="w-1/3 aspect-square"
         />
 
         <p className="font-bold">
-          Sorry... We could not find page you are looking for
+          Sorry... We could not find page you are looking for...
         </p>
         <Link to="/">
           <Button placeholder={undefined} className="bg-primary">
