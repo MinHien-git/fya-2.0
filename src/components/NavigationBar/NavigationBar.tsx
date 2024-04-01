5756cd9669735ce24e75e15fdda4992332b6d8d0
MinHien <64275316+MinHien-git@users.noreply.github.com>
2024-04-01 23:17:16 +0700

    update

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -12,6 +12,9 @@ import Cookies from "js-cookie";
 import { Signout } from "../../api/lib/authentication";
 import { useSelector, useDispatch } from "react-redux";
 import { setUser as setReduxUser } from "../../features/users/userSplice";
+import { Label, TextInput } from "flowbite-react";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { HiSearch } from "react-icons/hi";
 interface IUser {
   name?: string;
   email?: string;
@@ -69,33 +72,13 @@ export default function NavigationBar() {
               </div>
               <div className="flex-1 flex justify-center lg:ml-6 lg:w-[10rem]">
                 <div className="max-w-lg w-full lg:max-w-xs">
-                  <label htmlFor="search" className="sr-only">
-                    Search{" "}
-                  </label>
                   <form method="get" action="#" className="relative z-50">
-                    <button
-                      type="submit"
-                      id="searchsubmit"
-                      className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
-                    >
-                      <svg
-                        className="h-5 w-5 text-gray-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </button>
-                    <input
-                      type="text"
-                      name="s"
-                      id="s"
-                      className="block w-full pl-10 pr-3 py-2 border rounded-lg leading-5 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out"
-                      placeholder="Services (e.g Digital Marketing)"
+                    <TextInput
+                      id="email4"
+                      type="email"
+                      icon={HiSearch}
+                      placeholder="Search services here"
+                      required
                     />
                   </form>
                 </div>
